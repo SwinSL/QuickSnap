b1c1aef7eb950d845ea8fe700811a92044449fb5
Sheldon <100086149@students.swinburne.edu.my>
2019-03-14 17:48:17 +0800

    Added slap sounds
    Conflicts:
    	CardGames.csproj
    	src/SnapGame.cs

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -11,8 +11,15 @@ namespace CardGames
             Bitmap cards;
             cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
             SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+<<<<<<< HEAD
 			SwinGame.LoadFontNamed ("GameFont", "Chunk.otf", 24);
 		}
+=======
+			SwinGame.LoadSoundEffectNamed ("slap", "slap.wav");
+			SwinGame.LoadSoundEffectNamed ("slap1", "slap1.mp3");
+			SwinGame.LoadSoundEffectNamed ("slap2", "slap2.mp3");
+        }
+>>>>>>> 4c0793f... Added slap sounds
 
 		/// <summary>
 		/// Respond to the user input -- with requests affecting myGame
@@ -30,12 +37,14 @@ namespace CardGames
 
 			if (myGame.IsStarted) {
 				if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) && SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
-					//add sound effects
+					SwinGame.PlaySoundEffect ("slap");
 				}
 			} else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) {
 				myGame.PlayerHit (0);
+				SwinGame.PlaySoundEffect ("slap1");
 			} else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
 				myGame.PlayerHit (1);
+				SwinGame.PlaySoundEffect ("slap2");
 			}
 		}
 
